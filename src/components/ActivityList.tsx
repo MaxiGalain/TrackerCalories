dea091a2072feb603f4172727f795fd1c5dc2334
Maximiliano <galain.maximiliano@gmail.com>
2024-04-06 10:33:32 -0300

    Se realizaron algunas funcionalidades mas, entre ellas contador de calorias, de calorias quemadas y su diferencia. Agregado, edicion y eliminacion de actividades.

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,7 +1,7 @@
 import { Dispatch, useMemo } from "react"
 import { categories } from "../data/categories"
 import { Activity } from "../types"
-import { PencilSquareIcon }  from '@heroicons/react/24/outline'
+import { PencilSquareIcon, XCircleIcon }  from '@heroicons/react/24/outline'
 import { ActivityActions } from "../reducers/activity-reducer"
 
 type ActivityListProps = {
@@ -16,15 +16,17 @@ export default function ActivityList({activities, dispatch} : ActivityListProps)
    (category: Activity['category']) => categories.map(cat => cat.id === category ? cat.name : '')
   ,[activities])
 
-  console.log(activities)
+  const isEmptyActivities = useMemo(() => activities.length === 0 , [activities])
+
   return (
     <>
       <h2 className="text-4xl font-bold text-slate-600 text-center">
         Comidas y Actividades: 
       </h2>
 
-      {activities.map(activity => (
-        <div key={activity.id} className="px-5 py-10 bg-white mt-5 flex justify-between">
+      { isEmptyActivities ? <p className="text-center my-5">No hay actividades aun...</p> :
+        activities.map(activity => (
+        <div key={activity.id} className="px-5 py-10 bg-white mt-5 flex justify-between shadow">
           <div className="space-y-2 relative">
             <p className={`absolute -top-8 -left-8 px-10 py-2 text-white uppercase font-bold ${activity.category === 1 ? 'bg-lime-500' : 'bg-orange-500'}`}>
               {categoryName(+activity.category)}
@@ -33,7 +35,7 @@ export default function ActivityList({activities, dispatch} : ActivityListProps)
               {activity.name}
             </p>
             <p className="font-black text-4xl text-lime-500">
-              {activity.calories}{''}
+              {activity.calories}{' '}
               <span>Calorias</span>
             </p>
           </div>
@@ -41,6 +43,9 @@ export default function ActivityList({activities, dispatch} : ActivityListProps)
           <div className="flex gap-5 item-center">
             <button onClick={() => dispatch({type: "set-activeId", payload: {id: activity.id}})}>
               <PencilSquareIcon className="h-8 w-8 text-gray-800"/>
+            </button>
+            <button onClick={() => dispatch({type: "delete-activeId", payload: {id: activity.id}})}>
+              <XCircleIcon className="h-8 w-8 text-red-500"/>
             </button>
           </div>
         </div>
